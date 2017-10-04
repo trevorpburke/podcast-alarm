56dee6912d26e6d524b57b854e36abec3843bad6
Trevor Burke <trevorpburke@gmail.com>
2017-10-04 12:19:11 -0700

    Scraped the NPR website for podcast IDs and furthered the sscript to rely on .env file to control which podcast is being downloaded

--- alarm.py
+++ alarm.py
@@ -1,2 +1,16 @@
 import urllib
+import feedparser
+import json
+import os
+from podcasts import podcasts
 
+podcast = os.environ.get("PODCAST")
+
+feed=feedparser.parse(f'https://www.npr.org/rss/podcast.php?id={str(podcasts[podcast])}')
+items = feed['entries']
+podcast_date = items[0].title
+podcast_url = items[0].links[0].href
+
+# download file and save to archive
+urllib.request.urlretrieve(podcast_url,
+                           f'/home/tburke/npr-wakeup/archive/{podcast_date}.mp3')
