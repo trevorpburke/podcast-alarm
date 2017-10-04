29cf4bb8d5fcb9a32d980345157a188f1e339cb5
Trevor Burke <trevorpburke@gmail.com>
2017-10-04 16:38:51 -0700

    add user input sys.argv[1], create podcast specific archive folders

--- alarm.py
+++ alarm.py
@@ -1,12 +1,21 @@
-import urllib
+import sys
+import os
+from urllib.request import urlretrieve
 import feedparser
 from podcasts import podcasts
 
+podcast = sys.argv[1]
+
 feed=feedparser.parse(f'https://www.npr.org/rss/podcast.php?id={str(podcasts[podcast])}')
 items = feed['entries']
-podcast_date = items[0].title
+podcast_name = items[0].title
 podcast_url = items[0].links[0].href
 
 # download file and save to archive
-urllib.request.urlretrieve(podcast_url,
-                           f'/home/tburke/npr-wakeup/archive/{podcast_date}.mp3')
+if os.path.isdir(f'archive/{podcast}'):
+    urlretrieve(podcast_url, 
+                f'/home/tburke/podcast-alarm/archive/{podcast}/{podcast_name}.mp3')
+else:
+    os.mkdir(f'archive/{podcast}')
+    urlretrieve(podcast_url,
+                f'/home/tburke/podcast-alarm/archive/{podcast}/{podcast_name}.mp3')
