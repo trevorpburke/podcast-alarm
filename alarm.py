27b20902cd51665ab3e4f4e88f9b15b5a21588c1
Trevor Burke <trevorpburke@gmail.com>
2017-10-04 18:47:50 -0700

    Add pygame play/stop functionality... TBD if it works

--- alarm.py
+++ alarm.py
@@ -2,8 +2,10 @@ import sys
 import os
 from urllib.request import urlretrieve
 import feedparser
+from pygame import mixer
 from podcasts import podcasts
 
+# Use quotes around argument
 podcast = sys.argv[1]
 
 feed=feedparser.parse(f'https://www.npr.org/rss/podcast.php?id={str(podcasts[podcast])}')
@@ -19,3 +21,13 @@ else:
     os.mkdir(f'archive/{podcast}')
     urlretrieve(podcast_url,
                 f'/home/tburke/podcast-alarm/archive/{podcast}/{podcast_name}.mp3')
+
+# TODO play mp3
+
+def play_podcast(podcast):
+    mixer.init()
+    mixer.music.play(f'archive/{podcast}/{podcast_name}.mp3')
+    mixer.music.stop()
+
+if __name__ == '__main__':
+    play_podcast(podcast)
